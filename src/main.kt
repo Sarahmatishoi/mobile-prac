7947c71657e57aebd00c3870217cef59ba2787cd
sarahmatishoi <sarahmatishoi@gmail.com>
2021-03-11 13:48:37 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,20 @@
+fun main(){
+   add()
+    printName()
+    modulus()
+}
+fun add(){
+    var a=34
+    var b=102
+    var sum=a+b
+    println(sum)
+}
+fun printName(){
+    println("sarah papai")
+}
+fun modulus(){
+    var num1=45
+    var num2=13
+    var modulus=num1%num2
+    println(modulus)
+}
